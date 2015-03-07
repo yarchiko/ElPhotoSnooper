fb1c687b2b9bb4b190803aa8f989df39c4b408fe
Andrey Konstantinov <8ofproject@gmail.com>
2015-03-07 13:06:17 +0300

    Установка CocoaPods. Первичная настройка.

---
+++ ElPhotoSnooper/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  ElPhotoSnooper
+//
+//  Created by Mega on 07/03/15.
+//  Copyright (c) 2015 8of. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :ios, '7.0'
+	pod 'AFNetworking', '2.5.1'
+	pod 'InstagramKit', '3.5.0'
